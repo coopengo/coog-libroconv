9970c98b177caf1058ba4e9564b1085a79321032
Jérémy Mousset <jeremy.mousset@coopengo.com>
2025-09-09 11:26:57 +0200

    typo

--- convert_app.py
+++ convert_app.py
@@ -31,7 +31,7 @@ def call_libreoffice(output_format, out_dir, path, options):
         ascii_value = str(ord(separator))
         cmd += [
             f'--infilter="Text - txt - csv (StarCalc):{ascii_value},34,76,"']
-    # SECURIY: please never add shell=True to check_call
+    # SECURITY: please never add shell=True to check_call
     check_call(cmd, timeout=libreoffice_timeout)
 
 
